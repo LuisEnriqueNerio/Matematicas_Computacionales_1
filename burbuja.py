a6da275dc99cce684a3d31e2ccbc74e1aff8c61b
Luis Nerio <luiselopeznerio@gmail.com>
2017-09-10 00:29:04 -0500

    Update burbuja.py

--- burbuja.py
+++ burbuja.py
@@ -7,15 +7,31 @@ Created on Sat Aug 26 23:07:50 2017
 
 import random
 
+"""******************************************************************************************
+############################################################################################
+
+Función para crear números aleatorios, toma como parametros n, que es la cantidad de números
+que deseas y  lim_inf y lim_sup que son parametros para indicar el intervalo en el que deben
+estar los números aleatorios generados
+############################################################################################
+******************************************************************************************"""
 def ran_num(n,lim_inf=0, lim_sup=100):
-    arreglo = []
+    arreglo = []                  
     for i in range(n):
         arreglo.append(random.randint(lim_inf, lim_sup))
     return arreglo
 
 
+
+
+"""******************************************************************************************
+############################################################################################
+                                        Burbuja
+Función para ordenar un arreglo con algoritmo burbuja, toma como parametros un arreglo
+############################################################################################
+******************************************************************************************"""
 def burbuja(arreglo):
-    swap_count = 1
+    swap_count = 1                                                  ##Variable utilizada para saber cuantas veces se realiza un cambio de posición de valores
     while True:
         swap_count=0
         for i in range(len(arreglo)-1):
@@ -25,7 +41,13 @@ def burbuja(arreglo):
     return arreglo
         
 
-
+"""******************************************************************************************
+############################################################################################
+                                            swap
+Función que realiza un intercambio de posición de valores dentro de un arreglo, toma como parametros
+el arreglo  y los indices de los valores a cambiar
+############################################################################################
+******************************************************************************************"""
 def swap(arreglo, l,u):
     if arreglo[l]>arreglo[u]:
         aux=arreglo[l]
@@ -34,4 +56,15 @@ def swap(arreglo, l,u):
         return 1
     else:
         return 0
-    
+
+
+
+###########################################################################
+#Ejemmplo
+###########################################################################
+p = ran_num(100)
+print("Arreglo desordenado", p)
+print()
+burbuja(p)
+
+print("Arreglo ordenado" ,p)
